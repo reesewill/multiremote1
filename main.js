19f495d5935d2bf3bee460aa6a19825614d97d94
[IMJ] Will Reese <reesewill@gmail.com>
2018-05-31 11:31:20 +0900

    move js to js file

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 (() => {
-  console.log('hello world');
+  var h1 = document.querySelector('h1'), counter = 0;
+  setInterval(() => {
+    h1.innerHTML ++ '!';
+    counter++; //increment counter
+    console.log(counter); // log counter
+  }, 1000)
 })();
